8ca34441a8d6f3d40363523a559e43e1c1b0ee8c
BrunoViola <brunohenriquesilvaviola@gmail.com>
2025-01-08 10:45:47 -0300

    add: script que popula inicialmente a tabela Seção

---
+++ Script-Inserting-Data.sql
@@ -0,0 +1,16 @@
+INSERT ALL
+    INTO Secao(Nome) VALUES('Administração e Economia') --1
+    INTO Secao(Nome) VALUES('Autoajuda') --2
+    INTO Secao(Nome) VALUES('Educação e Didáticos') --3
+    INTO Secao(Nome) VALUES('Fantasia e Horror') --4
+    INTO Secao(Nome) VALUES('HQS e Mangás') --5
+    INTO Secao(Nome) VALUES('Infantil') --6
+    INTO Secao(Nome) VALUES('Importados') --7
+    INTO Secao(Nome) VALUES('Literatura e Ficção') --8
+    INTO Secao(Nome) VALUES('Religião e Espiritualidade') --9
+    INTO Secao(Nome) VALUES('Romance') --10
+    INTO Secao(Nome) VALUES('Saúde e Família') --11
+    INTO Secao(Nome) VALUES('Política e Filosofia') -- 12
+SELECT * FROM DUAL;
+
+select * from secao;
